b5e955aebb3f631ddd629bc50db2084d0a55ad8a
ritish78 <ritishtimalsina78@gmail.com>
2026-05-03 09:23:15 +0545

    Feature: Added property column and also added property info in seed

---
+++ Backend/src/models/property.ts
@@ -0,0 +1,49 @@
+import { InferSelectModel } from "drizzle-orm";
+import {
+  pgTable,
+  pgEnum,
+  index,
+  uuid,
+  varchar,
+  text,
+  boolean,
+  timestamp,
+  integer,
+} from "drizzle-orm/pg-core";
+import { address } from "./address";
+
+export const PropertyType = pgEnum("property_type", ["House", "Land"]);
+export const PropertyStatus = pgEnum("property_status", ["Sale", "Rent", "Hold", "Sold"]);
+
+export const property = pgTable(
+  "property",
+  {
+    id: uuid("id").primaryKey(),
+    title: varchar("title", { length: 255 }).notNull(),
+    slug: varchar("slug", { length: 255 }).notNull(),
+    description: text("description").notNull(),
+    toRent: boolean("to_rent").notNull(),
+    address: uuid("address").references(() => address.id),
+    closeLandmark: varchar("close_landmark", { length: 255 }),
+    propertyType: PropertyType("property_type").default("House").notNull(),
+    availableFrom: timestamp("available_from", { mode: "string" }).notNull(),
+    availableTill: timestamp("available_till", { mode: "string" }),
+    price: integer("price").notNull(),
+    negotiable: boolean("negotiable").default(false).notNull(),
+    imageUrl: text("image_url").array(),
+    status: PropertyStatus("status").default("Sale").notNull(),
+    listedAt: timestamp("listed_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+    featured: boolean("featured").default(false),
+    private: boolean("private").default(false),
+    expiresOn: timestamp("expires_on", { mode: "string" }).notNull(),
+    views: integer("views").default(1).notNull(),
+  },
+  (table) => [
+    index("address_index").on(table.address),
+    index("price_index").on(table.price),
+    index("property_type_index").on(table.price),
+  ],
+);
+
+export type Property = InferSelectModel<typeof property>;
